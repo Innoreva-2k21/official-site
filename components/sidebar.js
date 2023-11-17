a347ed7a3ef2928f6a6fd0bd87987ea59487f1f3
basundhara-070 <141000153+basundhara-070@users.noreply.github.com>
2023-11-17 22:14:52 +0530

    Update sidebar.js

--- components/sidebar.js
+++ components/sidebar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Sidebar = () => {
     return (
 <div className=" bg-[#050816]">
-      <div className="fixed w-[100px] h-[200px] my-0 p-0 top-0 left-0 rotate-90 [-webkit-text-stroke:2px_#ffffff80] [font-family:'Archivo-ExtraBold',Helvetica] font-extrabold text-transparent text-[80px] text-center tracking-[13.10px]">
+      <div className="absolute w-[6vw] h-[7vw] my-0 p-0 top-0 left-0 mt-4 ml-7 rotate-90 [-webkit-text-stroke:0.12rem_#ffffff80] [font-family:'Archivo-ExtraBold',Helvetica] font-extrabold text-transparent text-[4.8vw] text-center tracking-[0.8vw]">
       {`ALUMNI`}
 
       </div>
