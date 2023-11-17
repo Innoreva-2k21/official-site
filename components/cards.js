efe6f86127f65299c301243474b70fd567586a2b
basundhara-070 <141000153+basundhara-070@users.noreply.github.com>
2023-11-17 22:13:52 +0530

    Update cards.js

--- components/cards.js
+++ components/cards.js
@@ -1,43 +1,78 @@
+
+import React, { useEffect, useMemo } from 'react';
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import data from './data.json'
-function Cards() {
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 1
+import data from './data.json';
+
+const Cards = () => {
+  const settings =useMemo(()=> {
+    return{
+    dots: false,
+    infinite: true,
+    slidesToScroll: 1
     };
-    return (
-      <div className='w-4/5 ml-40'>
-        <div className="mt-20">
-        <Slider {...settings}>
-          {data.map((d) => (
-            <div className="inline-flex items-start gap-[10px] flex-[0_0_auto] relative">
-            <div className="w-[278px] h-[382px] rounded-[15px] bg-[#6e6e6e] relative" />
-            <div className="border-2 border-solid border-white w-[259px] left-[10px] top-[8px] h-[366px] rounded-[15px] absolute" />
-            <div className="w-[130px] left-[74px] top-[61px] h-[130px] rounded-[65px] absolute bg-cover bg-center" style={{ backgroundImage: `url(${d.img})` }}>
-          </div>
-            <div className="border-2 border-solid border-white w-[160px] left-[59px] top-[46px] h-[160px] rounded-[80px] absolute" />
-            <div className="[font-family:'Archivo-Bold',Helvetica] w-[65px] left-[107px] tracking-[0] text-[20px] top-[250px] text-white h-[32px] font-bold leading-[normal] absolute">
+  },[]);
+
+  // Function to dynamically set the number of slides to show based on screen width
+  const getSlidesToShow = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 1024) {
+      console.log(3);
+      return 4;
+       // Show 3 slides for screens larger than or equal to 1024px
+    } else if (screenWidth >= 768) {
+      console.log(2);
+      return 2; // Show 2 slides for screens larger than or equal to 768px
+    } else {
+      console.log(1);
+      return 1; // Show 1 slide for smaller screens
+    }
+  };
+
+  settings.slidesToShow = getSlidesToShow(); // Set initial slides to show
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Update slides to show when the window is resized
+      settings.slidesToShow = getSlidesToShow();
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [settings]);
+
+  return (
+    <div style={{ width: '82vw', marginLeft: '12vw' }}>
+  <div style={{ marginTop: '2vw' }}>
+    <Slider {...settings}>
+      {data.map((d) => (
+        <div style={{ display: 'flex', justifyContent: 'center' , gap: '0.1vw', flexWrap: 'wrap', position: 'relative' }}>
+          <div style={{ width: '17.375vw', height: '23.875vw', borderRadius: '0.9375vw', backgroundColor: '#6e6e6e', position: 'relative' }}>
+            {/* Additional components */}
+            <div style={{ width: '16.1875vw', left: '0.625vw', top: '0.5vw', height: '22.875vw', borderRadius: '0.9375vw', border: '2px solid white', position: 'absolute' }} />
+            <div style={{ width: '8.125vw', left: '4.625vw', top: '3.8125vw', height: '8.125vw', borderRadius: '4.0625vw', backgroundImage: `url(${d.img})`, backgroundSize: 'cover', backgroundPosition: 'center', position: 'absolute' }} />
+            <div style={{ width: '10vw', left: '3.6875vw', top: '2.875vw', height: '10vw', borderRadius: '5vw', border: '2px solid white', position: 'absolute' }} />
+            <div style={{ fontFamily: 'Archivo-Bold, Helvetica', width: '4.0625vw', left: '6.6875vw', letterSpacing: '0', fontSize: '1.25vw', top: '15.625vw', color: 'white', height: '2vw', fontWeight: 'bold', lineHeight: 'normal', position: 'absolute' }}>
               {d.name}
             </div>
-            <div className="[font-family:'Archivo-Medium',Helvetica] w-[142px] left-[68px] tracking-[0] text-[12px] top-[271px] text-white h-[32px] font-medium leading-[normal] absolute">
+            <div style={{ fontFamily: 'Archivo-Medium, Helvetica', width: '8.875vw', left: '4.25vw', letterSpacing: '0', fontSize: '0.75vw', top: '16.9375vw', color: 'white', height: '2vw', fontWeight: 'normal', lineHeight: 'normal', position: 'absolute' }}>
               {d.currentProfession}
             </div>
-            <p className="[font-family:'Archivo-Regular',Helvetica] w-[217px] left-[31px] tracking-[0] text-[11.5px] top-[294px] text-white h-[52px] font-normal text-center leading-[normal] absolute">
+            <p style={{ fontFamily: 'Archivo-Regular, Helvetica', width: '13.5625vw', left: '1.9375vw', letterSpacing: '0', fontSize: '0.6875vw', top: '18.375vw', color: 'white', height: '3.25vw', fontWeight: 'normal', textAlign: 'center', lineHeight: 'normal', position: 'absolute' }}>
               {d.review}
             </p>
           </div>
-          ))}
-        </Slider>
         </div>
-      </div>
-    );
-  }
-  
-  
-  
-  export default Cards;
+      ))}
+    </Slider>
+  </div>
+</div>
+
+  );
+}
+
+export default Cards;
+
