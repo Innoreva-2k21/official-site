44fb1b968e9fa5481ef96f6b28bc253867e52c7d
itsKaushiki <141013087+itsKaushiki@users.noreply.github.com>
2024-01-27 00:31:38 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,18 @@
+
+
 import React from 'react';
 
 const Sidebar = () => {
     return (
-<div className=" bg-[#050816]">
-      <div className="fixed w-[100px] h-[200px] my-0 p-0 top-0 left-0 rotate-90 [-webkit-text-stroke:2px_#ffffff80] [font-family:'Archivo-ExtraBold',Helvetica] font-extrabold text-transparent text-[80px] text-center tracking-[13.10px]">
-      {` ABOUT `}
-
-      </div>
-      <div className="fixed w-[100px] h-[200px] my-0 p-0 top-0 left-0 rotate-90 [-webkit-text-stroke:2px_#ffffff80] [font-family:'Archivo-ExtraBold',Helvetica] font-extrabold text-transparent text-[80px] text-center tracking-[13.10px]"style={{ marginTop: '450px' }}>
-      {`US`}
-      </div>
-    </div>
-   );
+        <div className="relative bg-[#050816]"style={{ marginLeft: '-40px' }}>
+            <div className="absolute w-[5rem] h-[10rem] my-0 p-0 top-150 left-50 rotate-90 [-webkit-text-stroke:2px_#ffffff80] [font-family:'Archivo-ExtraBold',Helvetica] font-extrabold text-transparent text-[80px] text-center tracking-[15.0px]"style={{ marginTop: '-50px' }}>
+                {` ABOUT `}
+            </div>
+            <div className="absolute w-[5rem] h-[10rem] my-0 p-0 top-150 left-50 rotate-90 [-webkit-text-stroke:2px_#ffffff80] [font-family:'Archivo-ExtraBold',Helvetica] font-extrabold text-transparent text-[80px] text-center tracking-[15.0px]" style={{ marginTop: '390px' }}>
+                {`US`}
+            </div>
+        </div>
+    );
 }
 
 export default Sidebar;
