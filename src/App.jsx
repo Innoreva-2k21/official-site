e0c9331ecf590fe5660b7fb73c2e041cc9d8e3bd
itsKaushiki <141013087+itsKaushiki@users.noreply.github.com>
2024-01-27 00:32:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,29 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex">
       <Sidebar />
-      <div className="fixed top-12 left-7 w-[28rem] h-[3.5rem]">
-        <div className="w-[28rem] h-[3.5rem] [font-family:'Archivo-SemiBold',Helvetica] font-semibold text-white text-[1.25rem] tracking-[0] leading-[2rem]" style={{ marginLeft: '-0.3125rem' }}>
-          WHO ARE WE?
-        </div>
-        <div className="w-[41.8125rem] h-[9.1875rem] [font-family:'Archivo',Helvetica] text-white text-[1rem] tracking-[0] leading-[1.5rem] text-justify" style={{ marginLeft: '9.375rem' }}>
+      <div className="flex-1 p-4 sm:p-6 md:p-8 lg:p-10 ml-[8rem]">
+      <div className="mb-6 text-[1.15rem] font-semibold text-white sm:text-[1.5rem] lg:text-[2rem] text-left">
+  WHO ARE WE?</div>
+  
+        <div className="mb-6 text-[0.8rem] text-white sm:text-[1.25rem] lg:text-[1.5rem] text-justify">
           Team Innoreva is a research-oriented team that places a strong emphasis on providing innovative and cutting-edge solutions to challenges faced by the real world.
         </div>
-        <div className="w-[28rem] h-[3.5rem] [font-family:'Archivo-SemiBold',Helvetica] font-semibold text-white text-[1.25rem] tracking-[0] leading-[2rem]" style={{ marginLeft: '2.5rem' }}>
+        <div className="mb-6 text-[1.15rem] font-semibold text-white sm:text-[1.5rem] lg:text-[2rem] text-left">
           WHAT’S OUR PURPOSE?
         </div>
-        <div className="w-[41.8125rem] h-[9.1875rem] [font-family:'Archivo',Helvetica] text-white text-[1rem] tracking-[0] leading-[1.5rem] text-justify" style={{ marginLeft: '9.375rem' }}>
+        <div className="mb-6 text-[0.8rem] text-white sm:text-[1.25rem] lg:text-[1.5rem] text-justify">
           Team Innoreva fosters a creative and innovative environment for developing groundbreaking solutions. We integrate various domains of technology and push the boundaries of technological advancements.
         </div>
-        <div className="w-[28rem] h-[3.5rem] [font-family:'Archivo-SemiBold',Helvetica] font-semibold text-white text-[1.25rem] tracking-[0] leading-[2rem]" style={{ marginLeft: '5rem' }}>
+        <div className="mb-6 text-[1.15rem] font-semibold text-white sm:text-[1.5rem] lg:text-[2rem] text-left">
           WHAT ARE OUR MAIN DOMAINS?
         </div>
-        <div className="w-[41.8125rem] h-[9.1875rem] [font-family:'Archivo',Helvetica] text-white text-[1rem] tracking-[0] leading-[1.5rem] text-justify" style={{ marginLeft: '9.375rem' }}>
+        <div className="text-[1rem] text-white sm:text-[1.25rem] lg:text-[1.5rem] text-justify">
           Since its formation in 2018, the team has been dedicated to exploring the domains of IOT, ROBOTICS, WEB/APP DEVELOPMENT, and MACHINE LEARNING/AI.
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
